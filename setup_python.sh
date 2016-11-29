c3bd384c2af1f8f143e44ab6e650f3b4e2f1d073
seking <seking.co@gmail.com>
2016-11-29 17:47:40 +0900

    mod setup

--- setup_python.sh
+++ setup_python.sh
@@ -1,10 +1,12 @@
 brew install pyenv-virtualenv
-cat >>~/.zshrc.local <<'EOT'
+setup_command=$(cat <<'EOT'
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 EOT
-exec $SHELL
+)
+echo $setup_command >> ~/.zshrc.local
+$setup_command
 pyenv install 3.5.0
 pyenv global 3.5.0
